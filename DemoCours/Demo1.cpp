0699fb421f2eb9c93a1ec4988cbf3bbaf3b10eab
Philou <ph.bovey@gmail.com>
2021-08-08 00:32:46 +0200

    Update Demo1.cpp
    just display a message on alias shell

--- DemoCours/Demo1.cpp
+++ DemoCours/Demo1.cpp
@@ -1,1 +1,24 @@
-// code sources en CPP
+//-----------------------------------------------------------------------------------//
+// Nom du projet 		: Demo Cours en Cpp
+// Nom du fichier 		: DemoCpp1.cpp
+// Date de création 	: 07.08.2021
+// Date de modification : xx.xx.2021
+//
+// Auteur 				: Philou (Ph. Bovey)
+//
+// Description          : Juste un "hello world" 
+//
+//
+// Remarques :           
+//----------------------------------------------------------------------------------//
+//-- librairie standart --//
+#include <iostream>
+
+//-- librairie perso --//
+
+//-- entrée du programme => main --// 
+void main(void)
+{
+	//-- affichage d'un msg en mode console --// 
+	std::cout << "hello les SLO2"; 
+}
